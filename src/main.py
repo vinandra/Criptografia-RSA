59c56a80b5786e0946d01391358a8c2eddab29a1
vinandra <vinandra207@gmail.com>
2023-11-25 18:31:16 +0700

    add: special key

--- src/main.py
+++ src/main.py
@@ -3,16 +3,21 @@
 print("++++++ Criptografi RSA ++++++++++")
 
 plaintex = input("masukan simbol = ") 
-p = int(input("masukan nilai p = "))
-q = int(input("masukan nilai q = "))
-# key = int(input("masukan key = "))
+p = int(input("masukan nilai p (bilangan prima)= "))
+q = int(input("masukan nilai q (bilangan prima)= "))
+e = int(input("masukan kunci umum (bilangan yang lebih prima)= "))
 
 n = p*q
 
 teta = (p-1)*(q-1)
 
-# Mengonversi simbol ke nilai ASCII
 nilai_ascii = ord(plaintex)
+
+for k in range(1, 100):
+    d = (1 + k * teta) / 19
+    if d.is_integer():
+        print(f"Nilai d yang memenuhi kondisi untuk k = {k}: {int(d)}")
+        break
 
 #output
 print(f"Plaintex : {plaintex}\nNilai n = {n}")
